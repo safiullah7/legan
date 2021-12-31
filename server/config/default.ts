cd723035dd048e766709d643289c402456927ba8
Safi Ullah <iamsafiullah@gmail.com>
2021-12-31 12:33:55 +0500

    updates

--- server/config/default.ts
+++ server/config/default.ts
@@ -4,10 +4,10 @@ export default {
     saltWorkFactor: 10,
     accessTokenTtl: '45m',
     refreshTokenTtl: "1y",
-    privateKey: `super-secret-key`,
-    CLOUDINARY_CLOUD_NAME: 'safiullah7',
-    CLOUDINARY_API_KEY: '158325367155989',
-    CLOUDINARY_API_SECRET: '_rHsD7jOd90wNV4J_limuc71L9g',
-    DB_URI: 'mongodb+srv://safi:safi@cluster0.oaal4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    privateKey: ``,
+    CLOUDINARY_CLOUD_NAME: '',
+    CLOUDINARY_API_KEY: '',
+    CLOUDINARY_API_SECRET: '',
+    DB_URI: '',
     SG_KEY: ''
 }
